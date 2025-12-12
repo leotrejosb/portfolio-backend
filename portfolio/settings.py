e94cd20e62ca82957ebcfcda9eeaefccc10cd7ac
leotrejosb <leonardo.trejos@utp.edu.co>
2025-12-12 14:25:43 -0500

    fixed update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Archivos subidos por usuarios (MEDIA)
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Initialize environment variables
 env = environ.Env()
